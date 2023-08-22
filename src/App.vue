43a3d37f25abdd9878c9c7fca85197096929eda4
DoubledConG <992196871@qq.com>
2023-08-22 21:29:03 +0800

    安装并使用Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/favicon.svg" class="logo" alt="Vite logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Computer Network Guide" />
+  <router-view></router-view>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+
+import router from './router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
