40d314086c7f159af98c4796d27e7638b59739f6
DoubledConG <992196871@qq.com>
2023-08-23 08:10:27 +0800

    引入Element Plus并完成基础布局

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,31 @@
 </script>
 
 <template>
-  <router-view></router-view>
+  <el-container>
+    <el-header>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
+        @select="handleSelect">
+        <el-menu-item index="0">LOGO</el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>Workspace</template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
+          <el-menu-item index="2-3">item three</el-menu-item>
+          <el-sub-menu index="2-4">
+            <template #title>item four</template>
+            <el-menu-item index="2-4-1">item one</el-menu-item>
+            <el-menu-item index="2-4-2">item two</el-menu-item>
+            <el-menu-item index="2-4-3">item three</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// import './style.css'
 
 import router from './router'
 
@@ -8,5 +12,7 @@ import App from './App.vue'
 const app = createApp(App)
 
 app.use(router)
+
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 app.mount('#app')
